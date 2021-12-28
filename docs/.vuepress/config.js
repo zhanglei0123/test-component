08cec3a0db5f59520b731c4a1c36efb461561322
zhanglei <Azalea.zhang@smzdm.com>
2021-12-28 18:05:50 +0800

    feat:save

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  title: 'test-ui',
   base: '/test-component/',
   themeConfig: {
+    nav: [{
+      text: 'Home', link: '/',
+    }, {
+      text: 'Github', link: 'https://githum.com/zhanglei0123/test-component'
+    }, {
+      test: 'Npm', link: 'https://www.npmjs.com/package/test-component-zl'
+    }],
     sidebar: [
       '/',
       'componentsDocs/card'
